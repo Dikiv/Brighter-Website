1f9d4587faff9b7a3ef0df88ae09df93e3228542
chris <crco@itu.dk>
2025-01-07 18:48:37 +0100

    functional  search

--- nextjs-dashboard/app/reviews/page.tsx
+++ nextjs-dashboard/app/reviews/page.tsx
@@ -1,8 +1,16 @@
 import { oswald } from '@/app/ui/fonts';
 import Reviewcard from './reviewCard';
+import SearchReview from './searchReview'
 
-export default async function Page() {
-    
+const Page = async({
+        searchParams,
+    }: {
+        searchParams?: Promise<{ query?: string }>
+
+    })=>{    
+
+    const resolved = await searchParams
+    const query = await resolved?.query ?? ''
 
     return (
         
@@ -13,17 +21,9 @@ export default async function Page() {
         <hr className="w-1/2 border-yellow-300 mb-4" />
 
         {/* Toolbar */}
-        <div className="border-x-2 border-yellow-300 w-1/3 bg-gray-900 shadow-md mt-4">
+        <div className="border-x-2 border-yellow-300 w-2/3 bg-gray-900 shadow-md mt-4">
             <div className="flex border-b-2 border-yellow-300 justify-around items-center py-1">
-                <button className="bg-yellow-300 text-gray-800 px-4 py-2 hover:bg-yellow-400">
-                    a
-                </button>
-                <button className="bg-yellow-300 text-gray-800 px-4 py-2 hover:bg-yellow-400">
-                    b
-                </button>
-                <button className="bg-yellow-300 text-gray-800 px-4 py-2 hover:bg-yellow-400">
-                    c
-                </button>
+                <SearchReview/>
             </div>
             <div className="flex justify-around items-center py-1">
                 <button className="bg-yellow-300 text-gray-800 px-4 py-2 hover:bg-yellow-400">
@@ -40,13 +40,12 @@ export default async function Page() {
         
         
         <div className="columns-sm space-y-3 gap-4 mx-auto mt-8 w-3/4 ">
-        <Reviewcard/>
+        <Reviewcard
+        query = {query}
+        />
         </div>
-    </div>
-
-    
-    
-
-        
+    </div> 
     );
 }
+
+export default Page
