bb5ba7c456134fc10f1452e6c8ea488385d902dd
Dikiv <80128352+Dikiv@users.noreply.github.com>
2025-01-13 00:04:49 +0100

    Env changes and card updates (#5)
    * repolist layout
    
    * fixed repo card pos
    
    * card adjustments
    
    ---------
    
    Co-authored-by: chris <crco@itu.dk>

--- nextjs-dashboard/app/works/RepoCard.tsx
+++ nextjs-dashboard/app/works/RepoCard.tsx
@@ -5,13 +5,18 @@ type RepoProps = {
 }
 
 export default function UserCard({ repo }: RepoProps) {
-  const x = 'https://github.com/' + repo.full_name
+  const repoURL = 'https://github.com/' + repo.full_name
+
   return (
-    <a href={x}>
-    <div  className='bg-blue-900 bg-gradient-to-b from-gray-800 border-yellow-700 to-black border-2 w-96 mt-6 p-3'>
-      <div className=''>{repo.full_name}</div>
+    <a onClick={()=>window.open(repoURL)}>
+    <div className='flex-initial sm:flex-row bg-blue-900 bg-gradient-to-b from-gray-800 border-yellow-700 hover:border-yellow-500 to-black border-2 mb-3 p-3'>
+      <div className=''>{repo.name}</div>
+      <hr className=" border-yellow-300 mb-4" />
       <div></div>
-      <div className='text-base'>{repo.url}</div>
+      <div className='text-base mb-4 '>{repo.description}
+      <img src="svgIcons/github-color.svg" alt="GitHub" className="float-right h-6 w-6" />
+      </div>
+      <div className='text-xs'>{repo.url}</div>
     </div>
     </a>
   )
--- nextjs-dashboard/app/works/page.tsx
+++ nextjs-dashboard/app/works/page.tsx
@@ -1,17 +1,20 @@
 import getRepos from './getRepos'
 import RepoList from './RepoList';
 
-const INITIAL_NUMBER_OF_REPOS = 7
+const INITIAL_NUMBER_OF_REPOS = 40
 
 export default async function Page() {
     const initialRepos = await getRepos(0, INITIAL_NUMBER_OF_REPOS)
     return (
     
-    <div className="flex grow p-8 items-center justify-center flex-col">
+    <div className="flex p-8 items-center justify-center flex-col">
             <h1 className={`text-center text-yellow-300 text-6xl leading-tight mb-4`}>
                 My Works
             </h1>
             <hr className="w-1/2 border-yellow-300 mb-4" />
+            <img src="svgIcons/github-color.svg" alt="GitHub" className="h-12 w-12 mb-4" />
+            <hr className="w-1/2 border-yellow-300 mb-4" />
+    
     <RepoList initialRepos={initialRepos}
     />
     </div>
